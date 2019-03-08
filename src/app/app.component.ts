2a81804583fe022ecfddd79be416a774eb325782
PirunSeng <sengpirun.rain@gmail.com>
2019-03-08 14:31:10 +0700

    Layout Demos - Static

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flex-layout';
+  dir = 'rtl';
+
+  toggleDirection(dir) {
+    this.dir = dir === 'rtl' ? 'ltr' : 'rtl'
+  }
 }
